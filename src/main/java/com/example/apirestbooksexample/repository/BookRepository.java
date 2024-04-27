db314a2540c36058aa5d55e5e129cae0e61b1473
samuelForDev <samuel.forerodev@gmail.com>
2024-04-27 18:53:32 -0500

    update: @repository in book repository

--- src/main/java/com/example/apirestbooksexample/repository/BookRepository.java
+++ src/main/java/com/example/apirestbooksexample/repository/BookRepository.java
@@ -4,10 +4,12 @@ import com.example.apirestbooksexample.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
 
     @Query("SELECT b FROM Book b WHERE b.author = :idAuthor")
