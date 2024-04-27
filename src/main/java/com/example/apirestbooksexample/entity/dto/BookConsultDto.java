1d192f3069d71388421d60cf47f844092dc0298d
samuelForDev <samuel.forerodev@gmail.com>
2024-04-27 18:52:46 -0500

    add: Dto consult and create book

---
+++ src/main/java/com/example/apirestbooksexample/entity/dto/BookConsultDto.java
@@ -0,0 +1,22 @@
+package com.example.apirestbooksexample.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookConsultDto {
+
+    private UUID idBook;
+    private String bookName;
+    private String pages;
+    private String author;
+    private String genre;
+
+}
---
+++ src/main/java/com/example/apirestbooksexample/entity/dto/BookCreateDto.java
@@ -0,0 +1,21 @@
+package com.example.apirestbooksexample.entity.dto;
+
+import com.example.apirestbooksexample.entity.Author;
+import com.example.apirestbooksexample.entity.Genre;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookCreateDto {
+
+    private String bookName;
+    private String pages;
+    private Author author;
+    private Genre genre;
+
+}
